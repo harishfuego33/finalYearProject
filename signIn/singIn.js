2b4b6bde5a6f83df9c034487ba368e1060112f4e
HarishkumarS33 <harishkumar.ct20@bitsathy.ac.in>
2023-08-21 13:15:25 +0530

    first commit

--- signIn/singIn.js
+++ signIn/singIn.js
@@ -1,12 +1,33 @@
 const EmailField = document.querySelector('.EmailField');
 const passwordField = document.querySelector('.passwordField');
-
-EmailField.addEventListener('focus', function(){
-  EmailField.removeAttribute('placeholder');
-});
-if(EmailField==null)
-EmailField.addAttribute('placeholder');
-
+const eyePng = document.querySelector('.eye-btn');
+let flag = true;
 passwordField.addEventListener('focus',function(){
-    passwordField.removeAttribute('placeholder');
+  eyePng.classList.remove('hidden');
+  flag = false;
 })
+eyePng.addEventListener('click',function()
+{
+  let img_src = document.querySelector(".eye-btn").src;
+  let src=img_src.split('/')[4]
+  let value = passwordField.value;
+  if(src ==='eye-close.png' && value.length>0)
+  {
+      eyePng.src = 'eye-open.png';
+      passwordField.type = 'text';  
+    }
+    else
+    {
+      eyePng.src = 'eye-close.png';
+      passwordField.type = 'password';
+    }
+})
+// if(passwordField.value.length==0&&!flag);
+// {
+//     document.addEventListener('click',function()
+//     {
+//       eyePng.classList.add('hidden'); 
+//     })
+// }
+
+
