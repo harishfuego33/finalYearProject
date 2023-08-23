7d61068533961321ca323a48493d5f1de0468459
HarishkumarS33 <harishkumar.ct20@bitsathy.ac.in>
2023-08-23 18:56:03 +0530

    first commit

--- signUp/signUp.js
+++ signUp/signUp.js
@@ -1,6 +1,7 @@
 const EmailField = document.querySelector('.EmailField');
 const passwordField = document.querySelector('.passwordField');
 const eyePng = document.querySelector('.eye-btn');
+const signUp =document.querySelector('.signUp');
 let flag = true;
 passwordField.addEventListener('focus',function(){
   eyePng.classList.remove('hidden');
@@ -21,4 +22,13 @@ eyePng.addEventListener('click',function()
       eyePng.src = 'eye-close.png';
       passwordField.type = 'password';
     }
+})
+signUp.addEventListener('click',function()
+{
+  console.log(passwordField.value);
+  if(passwordField.value=='harish@03'&&EmailField.value=='harishkumar.ct20@bitsathy.ac.in')
+  {
+    alert("log in sussesfully🤩");
+  }
+
 })
