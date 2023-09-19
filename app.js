b7aaa229d29ee69ab002f899adb9059f78fe72a8
harishfuego33 <harishkumar.ct20@bitsathy.ac.in>
2023-09-19 21:41:02 +0530

    updated

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ pool.connect((err) => {
     console.error('Error connecting to MySQL database: ' + err.stack);
     return;
   }
-  console.log('Connected to MySQL database as id ' + db.threadId);
+  console.log('Connected to MySQL database as id ' + pool.threadId);
 });
 
 
